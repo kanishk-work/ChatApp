600034d5de97011fdfc2b0f1db25464416489621
MayurNaik364 <mayur@wafer.ee>
2024-08-01 11:08:41 +0530

    UPDATE: Merged theme changes

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,27 +1,41 @@
-import { useAppDispatch, useAppSelector } from '../../redux/hooks'
-import { setShowProfile } from '../../redux/slices/profileSlice';
-import SideHeader from '../Shared/SideHeader';
+import { useAppDispatch, useAppSelector } from "../../Redux/hooks";
+import { setShowProfile } from "../../Redux/slices/profileSlice";
+import SideHeader from "../Shared/SideHeader";
 
 const Profile = () => {
-  const activeUser = useAppSelector((state) => state.activeUser)
+  const activeUser = useAppSelector((state) => state.activeUser);
   const dispatch = useAppDispatch();
-
 
   return (
     <div>
-      <SideHeader title='profile' backFn={() => dispatch(setShowProfile(false))} />
-      <div className='w-full flex items-center justify-center p-2 rounded-lg'>
-        <img src={activeUser.profilePic} alt="user profile pic" className='object-contain h-40 w-auto rounded-full flex-shrink-0' />
+      <SideHeader
+        title="profile"
+        backFn={() => dispatch(setShowProfile(false))}
+      />
+      <div className="w-full flex items-center justify-center p-2 rounded-lg">
+        <img
+          src={activeUser.profilePic}
+          alt="user profile pic"
+          className="object-contain h-40 w-auto rounded-full flex-shrink-0"
+        />
       </div>
 
-      <div id="userinfo" className='capitalize  text-lg'>
-        <div className='flex flex-col items-start gap-2 p-3 w-full hover:shadow-[0px_0px_20px_0px_#00000024] rounded-lg'>
-          <span className='dark:text-[var(--text-secondary)] text-[var(--text-secondary-light)]'>name</span>
-          <span className='dark:text-[var(--text-primary)] text-[var(--text-primary-light)]'>{activeUser.name}</span>
+      <div id="userinfo" className="capitalize  text-lg">
+        <div className="flex flex-col items-start gap-2 p-3 w-full hover:shadow-[0px_0px_20px_0px_#00000024] rounded-lg">
+          <span className="dark:text-[var(--text-secondary)] text-[var(--text-secondary-light)]">
+            name
+          </span>
+          <span className="dark:text-[var(--text-primary)] text-[var(--text-primary-light)]">
+            {activeUser.name}
+          </span>
         </div>
-        <div className='flex flex-col items-start gap-2 p-3 w-full hover:shadow-[0px_0px_20px_0px_#00000024] rounded-lg'>
-          <span className='dark:text-[var(--text-secondary)] text-[var(--text-secondary-light)]'>bio</span>
-          <span className='dark:text-[var(--text-primary)] text-[var(--text-primary-light)]'>{activeUser.bio}</span>
+        <div className="flex flex-col items-start gap-2 p-3 w-full hover:shadow-[0px_0px_20px_0px_#00000024] rounded-lg">
+          <span className="dark:text-[var(--text-secondary)] text-[var(--text-secondary-light)]">
+            bio
+          </span>
+          <span className="dark:text-[var(--text-primary)] text-[var(--text-primary-light)]">
+            {activeUser.bio}
+          </span>
         </div>
       </div>
     </div>
